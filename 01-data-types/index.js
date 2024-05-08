453f94d5acc4a1aa3813992f8aaa5835ec1cf91b
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-05-08 20:28:14 +0530

    first commit

---
+++ 01-data-types/index.js
@@ -0,0 +1,8 @@
+
+let firstName = 'samrat';
+let lastName = 'doe';
+let age = 25;
+
+firstName = 100;
+
+let user = 'my name is' + ' ' + firstName + ' ' + lastName + '. ' + 'i am' + ' ' + age + ' ' +'old'
