0095fceeef0d23a890f58d9ccf20779edbd785b0
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-05-09 17:05:23 +0530

    all

---
+++ 01-data-types/index2.js
@@ -0,0 +1,23 @@
+
+
+let str = "     i am samrat doe"      ;
+
+str.toLocaleLowerCase()
+
+//2nd step math operators
+
+13 % 2 //this gives you remainder
+5 ** 2 //this is square/cube depends upon last number after **
+
+
+//truthy or falsy
+
+// 0/-0
+// undefined
+// nan
+// null
+// empty('')
+
+
+
+
